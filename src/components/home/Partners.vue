d075d64111f539fc3a51bcab764cbc1d64f5b197
jackdishman <me@jackdishman.com>
2022-08-11 15:35:50 -0400

    contine on partners page

--- src/components/home/Partners.vue
+++ src/components/home/Partners.vue
@@ -3,10 +3,10 @@
 		<div>
 			<h2 class="uppercase text-rooRed text-3xl font-semibold italic text-center mb-5">PARTNERS OF THE TROOP</h2>
 			<div class="flex flex-row justify-center items-center">
-				<div class="px-5"><img :src="require(`@/assets/images/logos/asteria_labs.png`)" /></div>
-				<div class="px-5"><img :src="require(`@/assets/images/logos/eth_alien.png`)" /></div>
-				<div class="px-5"><img :src="require(`@/assets/images/logos/gac.png`)" /></div>
-				<div class="px-5"><img :src="require(`@/assets/images/logos/sappy_seals.png`)" /></div>
+				<div class="px-5"><img :src="require(`@/assets/images/logos/asteria_labs.webp`)" /></div>
+				<div class="px-5"><img :src="require(`@/assets/images/logos/eth_alien.webp`)" /></div>
+				<div class="px-5"><img :src="require(`@/assets/images/logos/gac.webp`)" /></div>
+				<div class="px-5"><img :src="require(`@/assets/images/logos/sappy_seals.webp`)" /></div>
 			</div>
 		</div>
 	</div>
--- src/pages/Partners.vue
+++ src/pages/Partners.vue
@@ -7,13 +7,58 @@ import RedUnderscore from '@/components/icons/RedUnderscore.vue'
 	<SubpageHeader :title="`Partnerships`" />
 	<div class="flex justify-center">
 		<div class="mb-10">
-			<!-- Partners -->
-			<div class="flex flex-row px-32">
-				<img :src="require(`@/assets/images/logos/sappy_seals.png`)" class="w-32 h-32" />
+			<!-- Sappy seals -->
+			<div class="flex flex-row px-32 mb-10">
+				<img :src="require(`@/assets/images/logos/sappy_seals.webp`)" class="w-32 object-contain flex-shrink-0" />
 				<div class="ml-16">
-					<h3>Sappy Seals</h3>
+					<h3 class="text-3xl font-semibold italic uppercase mb-3">Sappy Seals</h3>
 					<RedUnderscore />
-					<p>
+					<p class="pt-5">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.
+					</p>
+				</div>
+			</div>
+			<!-- eth aliens -->
+			<div class="flex flex-row px-32 mb-10">
+				<img :src="require(`@/assets/images/logos/eth_alien.webp`)" class="w-32 object-contain flex-shrink-0" />
+				<div class="ml-16">
+					<h3 class="text-3xl font-semibold italic uppercase mb-3">ethaliens</h3>
+					<RedUnderscore />
+					<p class="pt-5">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.
+					</p>
+				</div>
+			</div>
+			<!-- Sappy seals -->
+			<div class="flex flex-row px-32 mb-10">
+				<img :src="require(`@/assets/images/logos/asteria_labs.webp`)" class="w-32 object-contain flex-shrink-0" />
+				<div class="ml-16">
+					<h3 class="text-3xl font-semibold italic uppercase mb-3">asteria labs</h3>
+					<RedUnderscore />
+					<p class="pt-5">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.
+					</p>
+				</div>
+			</div>
+			<!-- GAC -->
+			<div class="flex flex-row px-32">
+				<img :src="require(`@/assets/images/logos/gac.webp`)" class="w-32 object-contain flex-shrink-0" />
+				<div class="ml-16">
+					<h3 class="text-3xl font-semibold italic uppercase mb-3">gac</h3>
+					<RedUnderscore />
+					<p class="pt-5">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
 						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
 						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
@@ -23,5 +68,8 @@ import RedUnderscore from '@/components/icons/RedUnderscore.vue'
 				</div>
 			</div>
 		</div>
+	</div>
+	<div class="flex justify-center">
+		<img :src="require(`@/assets/images/combo_logo.webp`)" alt="Logo" />
 	</div>
 </template>
